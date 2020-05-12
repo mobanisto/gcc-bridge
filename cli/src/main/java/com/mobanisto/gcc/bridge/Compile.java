83e2494c14ad055b260e5ea4da0993f6edcf7367
Sebastian Kürten <sebastian@topobyte.de>
2020-05-12 13:45:26 +0200

    Implement first version of a Compile exectuable

---
+++ cli/src/main/java/com/mobanisto/gcc/bridge/Compile.java
@@ -0,0 +1,35 @@
+package com.mobanisto.gcc.bridge;
+
+import de.topobyte.system.utils.SystemPaths;
+import org.renjin.gcc.Gcc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Compile {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            System.out.println("usage: Compile <output> <file...>");
+            System.exit(1);
+        }
+
+        Path pathPluginLibrary = SystemPaths.CWD.resolve("compiler/build/bridge.so");
+
+        Path output = Paths.get(args[0]);
+        if (!Files.exists(output)) {
+            Files.createDirectories(output);
+        }
+
+        Gcc gcc = new Gcc(output.toFile());
+        gcc.setPluginLibrary(pathPluginLibrary.toFile());
+
+        for (int i = 1; i < args.length; i++) {
+            Path file = Paths.get(args[i]);
+            gcc.compileToGimple(file.toFile());
+        }
+    }
+
+}
